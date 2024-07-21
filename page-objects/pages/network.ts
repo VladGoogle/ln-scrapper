7996c77d304a2ac04d1c5957c37a73a86e3da031
Vladislav Huhlia <vladislav.huhlia@it-dimension.com>
2024-07-21 21:40:36 +0300

    Update script

--- page-objects/pages/network.ts
+++ page-objects/pages/network.ts
@@ -5,8 +5,8 @@ export class NetworkPage {
     readonly growTab: Locator;
     readonly connectButtons: Locator;
     readonly hideChatButton: Locator;
-    readonly hideChatButton: Locator;
-    readonly limitPopup: Locator;
+    readonly limitLastPopup: Locator;
+    readonly limitWarningPopup: Locator;
     readonly limitPopupCloseButton: Locator;
 
     constructor(page: Page) {
@@ -14,7 +14,8 @@ export class NetworkPage {
         this.growTab = page.locator('xpath=/html/body/div[5]/div[3]/div/div/div/div/div[2]/div/div/main/div[1]/div/div/button[1]');
         this.connectButtons = page.locator('button[aria-label*="connect"]');
         this.hideChatButton = page.locator('xpath=/html/body/div[4]/div[4]/aside[1]/div[1]/header/div[3]/button[2]')
-        this.limitPopup = page.locator('#ip-fuse-limit-alert__header')
+        this.limitLastPopup = page.getByRole('heading', { name: 'You’ve reached the weekly' })
+        this.limitWarningPopup = page.locator('#ip-fuse-limit-alert__header')
         this.limitPopupCloseButton = page.getByLabel('Got it')
     }
 }
--- tests/main.spec.ts
+++ tests/main.spec.ts
@@ -42,20 +42,39 @@ test('Click on the "Connect" button for all mutual connections', async () => {
 
   await page.waitForTimeout(4000);
 
-  const buttons = await network.connectButtons.all()
+  let buttons = await network.connectButtons.all()
 
   await network.hideChatButton.click()
 
-  for (const button of buttons) {
-    await  button.scrollIntoViewIfNeeded()
-    await button.click()
-    await page.waitForTimeout(1500);
+  let isClosed = false
 
-    if(await network.limitPopup.isVisible()) {
-      await network.limitPopupCloseButton.click()
-      await page.waitForTimeout(1000);
+  while (true) {
+
+    for (const button of buttons) {
+      await  button.scrollIntoViewIfNeeded()
+      await button.click()
+
+      if(await network.limitLastPopup.isVisible()) {
+        isClosed = true
+        break
+      }
+
+      await page.waitForTimeout(1500);
+
+      if(await network.limitWarningPopup.isVisible()) {
+        await network.limitPopupCloseButton.click()
+        await page.waitForTimeout(1000);
+      }
     }
 
+    if(isClosed) {
+      break
+    }
+
+    await page.mouse.wheel(0, 15000);
+    await page.waitForTimeout(3000);
+    buttons = await network.connectButtons.all()
   }
+
 
 });
